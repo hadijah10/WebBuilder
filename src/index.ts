36251df9779f47e0d67c035cb7abc927ef53cc76
Your Name <mohamaduhadijatu@gmail.com>
2025-05-29 08:04:07 -0400

    git check

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import "./style.scss";
-import data from "./assets/index.json";
-import { tool } from "./tools";
-const main = document.querySelector("main") as HTMLElement;
-const theme = document.querySelector(".theme") as HTMLElement;
-theme.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
-  if (document.body.classList.contains("dark-theme")) {
-    theme.style.justifyContent = "flex-end";
-    theme.style.backgroundColor = "rgba(0,0,0,70%)";
+import './style.scss';
+import data from './assets/index.json';
+import { tool } from './tools';
+const main = document.querySelector('main') as HTMLElement;
+const theme = document.querySelector('.theme') as HTMLElement;
+theme.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+  if (document.body.classList.contains('dark-theme')) {
+    theme.style.justifyContent = 'flex-end';
+    theme.style.backgroundColor = 'rgba(0,0,0,70%)';
   } else {
-    theme.style.justifyContent = "flex-start";
-    theme.style.backgroundColor = "#ffffff";
+    theme.style.justifyContent = 'flex-start';
+    theme.style.backgroundColor = '#ffffff';
   }
 });
 data.forEach((element: tool) => {
-  const tool = document.createElement("div") as HTMLDivElement;
+  const tool = document.createElement('div') as HTMLDivElement;
   tool.innerHTML = `
     <h2>${element.name}</h2>
     <p>Released on ${element.released}</p>
